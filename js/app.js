84e6a556e92e4daeffe98b70d036d0a596235276
Andrew Conway <ajc@countingthoughts.com>
2013-09-01 10:07:01 +0100

    First commit of code

---
+++ js/app.js
@@ -0,0 +1,40 @@
+var question = new Object();
+question.text="There's been a problem loading";
+question.correctAnswer="none";
+
+$(function() {
+});
+
+$(document).ready(function(){
+  loadFromAPI();
+  $("#buttonOK").click(function(){
+    answer = $("#answer").val();
+    console.log("Got to here");
+    if(answer == question.correctAnswer) {
+      $("#feedback").html("That's correct!");
+    } else {
+      $("#feedback").html("Wrong, loser!");
+    }
+  });
+});
+
+function load(){
+  q = new Object();
+  q.text="Does this work?";
+  q.correctAnswer="yes";
+  return q;
+}
+
+function loadFromAPI(){
+  var xhr = new XMLHttpRequest({mozSystem: true});
+  xhr.open("GET", "http://localhost/nalu/api.php?action=get_question", true);
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState == 4) {
+      console.log(xhr.responseText);
+      question = JSON.parse(xhr.responseText);
+      console.log(question.text);
+      $("#question").html(question.text);
+    }
+  }
+  xhr.send();
+}
